c0a2fc1e0730581a375364dd053dc5abd71c8a85
Ragib-hasin <ragibhasin295@gmail.com>
2026-01-09 21:43:10 +0600

    Add JWT

---
+++ helpers/verifyUser.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+function verifyUser(req, res, next) {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({ message: "Login required" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = decoded.id;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+}
+
+module.exports = verifyUser;
