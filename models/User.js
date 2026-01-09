d9e74449ed8cb7f6f260bb0ee64f776b689bff8e
Ragib-hasin <ragibhasin295@gmail.com>
2026-01-09 21:40:05 +0600

    Add User

---
+++ models/User.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    NIDNumber: { type: String, required: true, unique: true },
+    phoneNumber: { type: String, required: true, unique: true },
+    bloodGroup: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
