3e9bac1d8e3083b34d4c327b6166dbe99df2e15c
Ragib-hasin <ragibhasin295@gmail.com>
2026-01-09 21:50:37 +0600

    Add resgister user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,3 +5,34 @@ const User = require("../models/UserModel");
 const verifyUser = require("../helpers/verifyUser");
 
 const router = express.Router();
+
+//Register a new user
+
+router.post("/register", async (req, res) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      NIDNumber,
+      phoneNumber,
+      password,
+      bloodGroup,
+    } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      firstName,
+      lastName,
+      NIDNumber,
+      phoneNumber,
+      bloodGroup,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
